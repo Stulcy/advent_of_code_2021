4a3b12d5e5777cf84d89e2ec3f00eaad08193eca
Stulcy <rokkstular@gmail.com>
2021-12-05 13:00:16 +0000

    day2

---
+++ day2/day2.py
@@ -0,0 +1,29 @@
+horizontal = 0
+depth = 0
+
+for instruction, direction in (((item.split(' ')) for item in [str(instruction.strip('\n'))
+                                                               for instruction in open('day2/day2.txt').readlines()])):
+    if instruction == 'up':
+        horizontal -= int(direction)
+    elif instruction == 'down':
+        horizontal += int(direction)
+    else:
+        depth += int(direction)
+
+print(horizontal * depth)
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for instruction, direction in (((item.split(' ')) for item in [str(instruction.strip('\n'))
+                                                               for instruction in open('day2/day2.txt').readlines()])):
+    if instruction == 'up':
+        aim -= int(direction)
+    elif instruction == 'down':
+        aim += int(direction)
+    else:
+        horizontal += int(direction)
+        depth += (int(direction) * aim)
+
+print(horizontal * depth)
