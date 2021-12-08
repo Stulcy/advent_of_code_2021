35c46f39fd91fdb6b587f87c6a81dd1fecc80db3
Stulcy <rokkstular@gmail.com>
2021-12-08 00:14:21 +0000

    day7

---
+++ day7/day7.py
@@ -0,0 +1,23 @@
+crabship_positions = [int(position) for position in open(
+    'day7/day7.txt').readline().split(',')]
+
+
+def get_best_position(normal_consumption):
+    min_fuel_consumption = float('inf')
+    curr_best_position_fuel_consumption = 0
+    curr_best_position = 0
+    for possible in range(max(crabship_positions) + 1):
+        fuel_spent = 0
+        for crabship_position in crabship_positions:
+            fuel_spent += abs(possible - crabship_position) if normal_consumption else sum(
+                range(abs(possible - crabship_position) + 1))
+        if fuel_spent < min_fuel_consumption:
+            min_fuel_consumption = fuel_spent
+            curr_best_position_fuel_consumption = fuel_spent
+            curr_best_position = possible
+    print(curr_best_position)
+    print(curr_best_position_fuel_consumption)
+
+
+get_best_position(True)
+get_best_position(False)
